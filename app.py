a742a275ece5b883e06da5288b8d70973007192a
Kevin Lin <kevin.lin@thespurgroup.com>
2020-07-14 11:01:24 -0700

    adding basic html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, flash, jsonify, send_file
+from flask import Flask, request, redirect, url_for, flash, jsonify, send_file, render_template
 import numpy as np
 import pickle as p
 import json
@@ -7,6 +7,11 @@ from docx import Document
 from docx.shared import Inches
 
 app = Flask(__name__)
+
+### Testing out HTML
+@app.route('/')
+def form():
+  return render_template('index.html')
 
 ### THIS ONE WORKS WITH FORM-DATA
 @app.route('/api/v1/resources/text/', methods=['POST'])
@@ -41,4 +46,4 @@ def return_document():
     return send_file('datafile.docx', attachment_filename='test.docx')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
